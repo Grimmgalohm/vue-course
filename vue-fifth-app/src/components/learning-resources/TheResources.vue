e317e2e199e669b42e633f0ec269cdefad62347f
cesar_luna <cesarlunalimon@gmail.com>
2022-09-28 23:24:22 -0500

    fifth app vue

---
+++ vue-fifth-app/src/components/learning-resources/TheResources.vue
@@ -0,0 +1,18 @@
+<template>
+    <base-card>
+        <base-button @click="setSelectedTab('stored-resources')"></base-button>
+        <base-button @click="setSelectedTab('add-resource')"></base-button>
+    </base-card>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            selectedTab: 'stored-resources'
+        };
+    }
+}
+</script>
+
+<style></style>
